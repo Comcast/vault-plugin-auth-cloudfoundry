dadfd90423d54781404954b01a379bf792d20861
Adam Williams <Adam_Williams@comcast.com>
2019-02-18 10:59:46 -0700

    Update compose env to work with local source

--- dev/plugin/entrypoint/entrypoint.bash
+++ dev/plugin/entrypoint/entrypoint.bash
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-sleep 1
+# Copy build artificats to plugin directory. This is necessary
+# since the plugin directory is a mounted volume
+find ${BUILD_DIR} -executable -type f | while read p; do
+	cp "${p}" ${VAULT_PLUGIN_DIR}/.
+done
 
 vault login "${VAULT_TOKEN}" >/dev/null 2>&1
 
+if [ -z "${VAULT_PLUGIN_DIR}" ]; then
+	echo "\$VAULT_PLUGIN_DIR is not defined"
+	exit 1
+fi
+echo "Registering plugins in \"${VAULT_PLUGIN_DIR}\""
+
 # Register plugin(s)
-for p in ${VAULT_PLUGIN_DIR}/*; do
+find ${VAULT_PLUGIN_DIR} -executable -type f | while read p; do
 	# Ensure plugin is executable
 	if [[ -x "${p}" ]]; then
 		pluginHash=$(sha256sum "${p}" | head -c 64)
@@ -33,4 +43,5 @@ for p in ${VAULT_PLUGIN_DIR}/*; do
 done
 
 # Keep process alive
+echo "Entering eternal slumber..."
 sleep 1024d
